5c8983711cf6cd0d51ce522ed1f97e7ca86c8949
Divya Dua <divya.dua@knoldus.in>
2017-08-05 01:08:09 +0530

    Uploaded Session And Flash Scope Assignment

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController extends Controller {
+
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index = Action { implicit request: Request[AnyContent] =>
+    Ok("Hello World").withSession("Yes" -> "Session started for user")
+  }
+
+  /*def indexAction(id: Int) = Action { implicit request: Request[AnyContent] =>
+    Redirect(routes.HomeController.action2(id.toString)).withSession("Yes" -> s"User with id: $id")
+  }*/
+
+  def action1() = Action{
+    request =>
+      request.session.get("Yes").map { user =>
+       Ok(request.flash.get("success").getOrElse("No user found "))
+      }.getOrElse {
+        Unauthorized("Oops : No user")
+      }
+  }
+
+  def action2(user: String) = Action{
+    Redirect(routes.HomeController.action1()).flashing(
+      "success" -> s"We have got a user $user")
+  }
+}
